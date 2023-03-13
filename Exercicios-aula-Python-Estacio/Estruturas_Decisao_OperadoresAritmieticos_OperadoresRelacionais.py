895d17743f91e3b830404a036bef485e289ac0f9
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2023-03-13 19:25:54 -0300

    Add files via upload

---
+++ Exercicios-aula-Python-Estacio/Estruturas_Decisao_OperadoresAritmieticos_OperadoresRelacionais.py
@@ -0,0 +1,23 @@
+#Estrutas de decisao
+#São estruturas que servem para apresentar..
+#..algum resultado mediante algum teste logico SINTAXE
+
+#EXEMPLO 1:
+if (teste_logico):
+    boloco_verdadeiro
+
+#EXEMPLO 2:
+if (teste_logico):
+    blocoVerdadeiro
+else:
+    bloco_falso
+
+#Operadores Aritiméticos:
++ somar             * Produto
+- Diferença         / Divisão
+// Divisão Inteira  % Resto
+
+#Operadores Relacionais
+> maior que          < menor que
+>= maior ou igual    <= menor ou igual
+== igualdade         = Atribuição
