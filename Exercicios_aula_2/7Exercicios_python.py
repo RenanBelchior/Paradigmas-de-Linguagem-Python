8bf4433db233e83b5be78e894a6a9354239493cc
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2023-03-13 20:30:56 -0300

    Add files via upload

---
+++ Exercicios_aula_2/7Exercicios_python.py
@@ -0,0 +1,28 @@
+#Exercício:
+#1- Ler um numero inteiro e verificar se é PAR/IMPAR.
+
+#2- Ler um numero inteiro e verificar se é POSITIVO/NEGATIVO..
+#..(Considerando 0 positivo)
+
+#3- Leia os valores A,B,C e verifique se a soma A+B é..
+#..igual a C
+
+#4- Leia os numeros de gols dos 2 times, o nome deles..
+#..e informe se empataram ou qual dos 2 ganhou.
+
+#5- Leia o valor de um produto e calcule seu aumento..
+#.. de 10% imprima o preço final
+
+#6- Leia as opções de um usuario e dois numeros reais e..
+#.. faça os seguintes calculos, de acordo com a escolha..
+#.. do usuario
+1-Soma dos numeros
+2-Sbtraia os numeros
+3-Multiplica os numeros
+
+#7- Faça um programa python que leia o preço de um...
+#..produto e calcule o desconto de acordo com a forma..
+#..de pagamento escolhida pelo usuario
+1-À vista 15% desconto
+2-Cartão 18% desconto
+3-Pix 20% desconto
