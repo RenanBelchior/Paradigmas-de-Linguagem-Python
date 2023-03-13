839fb371a78a78016996196abf43e371fb699fa1
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2023-03-13 19:54:08 -0300

    Add files via upload

---
+++ Exercicios-aula-Python-Estacio/exemplo2_estrutura_decisao.py
@@ -0,0 +1,11 @@
+#Exemplo 2:
+
+nota1 = float(input("Nota1:"))
+nota2 = float(input("Nota2:"))
+
+media = (nota1 + nota2) / 2
+
+if(media>=6):
+    print("Aprovado!")
+else:
+    ("Reprovado!")
---
+++ Exercicios-aula-Python-Estacio/exemplo_1_estrutura_decisao.py
@@ -0,0 +1,8 @@
+#Exemplo 1:
+
+num=int(input('Digite:'))
+
+if(num >=100):
+    print("É maior ou igual a 100")
+else:
+    print("Não é maior que 100")
