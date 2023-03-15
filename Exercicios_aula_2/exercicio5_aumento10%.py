410b6aa3d8a8265d0e633862e43d72d4f1ba8d65
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2023-03-14 22:58:40 -0300

    Add files via upload

---
+++ Exercicios_aula_2/exercicio5_aumento10%.py
@@ -0,0 +1,9 @@
+#5- Leia o valor de um produto e calcule seu aumento..
+#de 10% imprima o preço final
+
+prod = float(input("Digite o valor do produto: "))
+
+aumento = prod * (10/100)
+prodFinal = prod + aumento
+
+print("O valor do produto com aumento de 10% é: ", prodFinal)
