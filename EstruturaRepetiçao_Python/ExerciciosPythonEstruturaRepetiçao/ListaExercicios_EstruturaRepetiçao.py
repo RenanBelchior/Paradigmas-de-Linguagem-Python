b9e630d0d5e80c42ea88f88674be857e26586204
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2023-03-20 19:53:43 -0300

    Add files via upload

---
+++ EstruturaRepetiçao_Python/ExerciciosPythonEstruturaRepetiçao/ListaExercicios_EstruturaRepetiçao.py
@@ -0,0 +1,15 @@
+#EXERCICIOS
+1- Imprima todos os números ímpares entre 1 e 150, usando..
+for e while.
+
+2- Imprima todos os numeros no intervalo entre 1 e N.
+
+3- Some todos os números entre 1 e 50
+
+4- Calcule a media eritimetica de N notas de um aluno..
+informado no teclado
+
+5- Some todos os numeros multiplos de 3 entre 1 e 50
+
+6- Conte quantos numeros multiplos de 5 existem entre..
+20 e 200.
