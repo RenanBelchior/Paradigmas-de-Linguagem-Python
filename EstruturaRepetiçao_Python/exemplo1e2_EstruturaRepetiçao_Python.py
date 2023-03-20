c7a8662e94c2033f354fb03d9a36420f23a71fe5
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2023-03-20 19:24:33 -0300

    Add files via upload

---
+++ EstruturaRepetiçao_Python/exemplo1e2_EstruturaRepetiçao_Python.py
@@ -0,0 +1,13 @@
+#Exemplo 1 Estrutura de repetição
+# Iterar sobre conjuntos
+
+nome = "Pyhton"
+
+for i in nome:  #'i' Variavel | 'in' Operador auxiliar
+    print(i)    #':' iniciador de bloco
+
+#Exemplo 2.
+nome = [2, 34, 22, 89]
+
+for i in nome:
+    print(i)
