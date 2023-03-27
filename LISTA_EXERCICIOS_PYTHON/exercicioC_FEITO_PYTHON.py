bf2e7d71029cb654d0636ef89a6dacc6d02c586d
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2023-03-27 20:10:05 -0300

    Add files via upload

---
+++ LISTA_EXERCICIOS_PYTHON/exercicioC_FEITO_PYTHON.py
@@ -0,0 +1,8 @@
+#c) Faça um programa em linguagem Python que converta metros para 
+# centímetros.
+
+metros = float(input("Digite quantos metros possui: "))
+
+centimetros = metros * 100
+
+print(centimetros, "cm")
