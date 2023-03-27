9edbb1bb69bbd81baec1188cc4a6865d9b1ba11a
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2023-03-27 20:02:58 -0300

    Add files via upload

---
+++ LISTA_EXERCICIOS_PYTHON/exercicioB_FEITO_PYTHON.py
@@ -0,0 +1,10 @@
+#b) Escreva um algoritmo para ler as dimensões de um retângulo 
+# (base e altura), calcular e
+# escrever a área do retângulo (𝐴 = 𝑏. ℎ).
+
+base = int(input("Digite a base do Retângulo: "))
+altura = int(input("Digite a Altura do Triângulo: "))
+
+areaTriangulo = base * altura
+
+print("A área do Triângulo é: ", areaTriangulo)
