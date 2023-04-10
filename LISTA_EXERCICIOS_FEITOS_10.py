25e3e3dc4776a0f91357d1b0dc0518e62047ff84
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2023-04-10 20:57:34 -0300

    Add files via upload

---
+++ LISTA_EXERCICIOS_FEITOS_10.py
@@ -0,0 +1,107 @@
+#Exercício 1- Ler um número inteiro e imprima a tabuada deste número.
+#print(".::Sistema de Tabuada::.")
+#num = int(input("Digite um número: "))
+
+#for count in range(10):
+    #print("%d x %d = %d" % (num, count+1, num*(count+1)))
+    
+    
+    
+#Exercicio 2- Ler a idade de uma pessoa e ver se ela é maior ou igual
+# a 18 anos.
+#pessoa = int(input("Digite sua idade: "))
+
+#if(pessoa < 18):
+    #print("Você é menor de idade")
+#elif(pessoa == 18):
+    #print("Você possui 18 anos!")
+#elif(pessoa > 18):
+    #print("Você é maior que 18 anos")
+    
+    
+    
+#Exercicio 3- Leia um valor para Grau Celsius e converta para Fahreheit
+#print(".::Sistema Graus Celsius para Fahreheit::.")
+#grauC = int(input("Digite um valor: "))
+#conversao = 9 / 5 * grauC + 32
+#print("Valor em Graus Celsius....: ", grauC)
+#print("Valor em Fahreheit........: ", conversao)
+
+
+
+#Exercicio 4- Ler uma String e imprimir seu Comprimento
+#def tamanho(x):
+    #print(f"Tamanho: {len(x)}")
+
+#palavra = input("Palavra: ")
+#tamanho(palavra)
+
+
+
+
+#exercicio 5- Ler uma sequencia de numeros inteiros e somar todos eles
+#print("..::Sistema Ler Sequência Números e a Soma Total::..")
+#numV = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#soma1 = 0;
+#for somar in numV:
+    #soma1 += somar
+#print(soma1)
+
+
+
+
+#Exercicio 6- Qual função da palavra reservada input() em python?
+#Resposta: B
+#a
+#b Ler um valor de entrada do usuário
+#c
+#d
+
+
+
+#Exercicio 7- Ler sequência de numeros e imprimir o maior e menor valor
+#opcao = "S"
+#soma, i = 0 , 1
+#menor, maior = 9999, -9999
+#while(opcao == "S" or opcao == "s"):
+    #print(f"Digite o {i}º número: ")
+    #n = int(input())
+    #if( n < menor):
+        #menor = n
+    #if(n > maior):
+        #maior = n
+    #i = i + 1
+    #opcao = input("Deseja Continuar?[S/N]: ")
+    #print()
+#print(f"Menor:{menor}")
+#print(f"Maior:{maior}")
+
+
+
+#Exercicio 8 - Criar programa calcule fatorial de numero n
+#de forma recursiva
+#def fatorial(n):
+    #if (n == 1):
+        #return 1
+    #else:
+        #return n * fatorial(n - 1)
+
+#n = int(input("N: "))
+#x = fatorial(n)
+#print("Fatorial: ", x)
+
+
+
+#Exercicio 9 - desenvolva funçao recursiva para imprimir numeros N a 1
+# ordem descrescente.
+#def imprimir(n):
+    #if (n == 1):
+        #return 1
+    #else:
+        #print(n)
+        #return imprimir(n - 1)
+
+#n = int(input("N: "))
+#print(imprimir(n))
+
+
